115420c7860d3355ba13d476cbd5cfdf6be60b94
obaraelijah <elijahobara357@gmail.com>
2023-02-28 19:21:07 +0000

    A function that deletes an entire binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,13 +6,13 @@
  *
  * Return: Nothing.
  */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return;
+	if (tree)
 	{
-		binary_tree_delete(left);
-		binary_tree_delete(right);
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
 		free(tree);
 	}
 }
