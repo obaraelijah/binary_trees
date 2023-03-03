19ae4ecff36685859a50801cf0438f1707208048
obaraelijah <elijahobara357@gmail.com>
2023-03-03 14:02:51 +0000

    Function that rotates a binary tree to the left

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -6,14 +6,34 @@
  *
  * Return: The new root node.
  */
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree) {
-    if (tree == NULL || tree->right == NULL) {
-        return (tree);
-    }
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *root;
+	binary_tree_t *new_root;
+	binary_tree_t *new_root_lc;
 
-    binary_tree_t *new_root = tree->right;
-    tree->right = new_root->left;
-    new_root->left = tree;
-
-    return (new_root);
+	if (tree == NULL)
+		return (NULL);
+	root = tree;
+	new_root = root->right;
+	if (new_root != NULL)
+	{
+		new_root->parent = root->parent;
+		new_root_lc = new_root->left;
+		new_root->left = root;
+		if (root->parent != NULL)
+		{
+			if (root->parent->left == root)
+				root->parent->left = new_root;
+			if (root->parent->right == root)
+				root->parent->right = new_root;
+		}
+		root->right = new_root_lc;
+		root->parent = new_root;
+		if (new_root_lc != NULL)
+		{
+			new_root_lc->parent = root;
+		}
+	}
+	return (new_root);
 }
